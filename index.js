c74f078ead5951b8334e08972480eb101f4b659c
mgreen1092 <morgan.green0209@gmail.com>
2022-12-02 15:40:03 -0500

    add index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const libraryController = require('./controllers/library')
 const app = express();
 
 const mongoose = require('mongoose')
@@ -10,6 +11,8 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/library', libraryController)
 
 app.listen(4000, () => {
   console.log('listening on port 4000');
