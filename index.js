c17af6b676165a34b27d4b916055ddd51eac7b15
mgreen1092 <morgan.green0209@gmail.com>
2022-12-02 15:46:14 -0500

    console.log(hi) index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/library', libraryController)
 
+console.log('hi')
+
 app.listen(4000, () => {
   console.log('listening on port 4000');
 });
